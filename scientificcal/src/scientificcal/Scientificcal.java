6af161f3a284042c55c449acb44f4419eb1ba5f3
mdhasanali3 <43574156+mdhasanali3@users.noreply.github.com>
2019-04-10 09:34:44 +0600

    Add files via upload

---
+++ scientificcal/src/scientificcal/Scientificcal.java
@@ -0,0 +1,13 @@
+
+package scientificcal;
+
+public class Scientificcal {
+
+    public static void main(String[] args) {
+        calframe frame=new calframe();
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        
+    }
+    
+}
